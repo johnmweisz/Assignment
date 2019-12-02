a88c02d5c94c48d4d3b5be6a5a9f852a6878039d
John Weisz <johnmweisz@gmail.com>
2019-12-02 15:46:18 -0800

    fixed issue wit docker https redirect

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace Assignment
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection(); Removed for Docker
             app.UseStaticFiles();
 
             app.UseRouting();
