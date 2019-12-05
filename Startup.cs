cc6d46f5e0384f98b27387cac1ca24a1a07f05ba
John Weisz <johnmweisz@gmail.com>
2019-12-05 11:13:26 -0800

    api now points to localhost:5001 for dotnet run

--- Startup.cs
+++ Startup.cs
@@ -42,15 +42,10 @@ namespace Assignment
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            // app.UseHttpsRedirection(); Removed for Docker
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-            app.UseCors();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
 
             app.UseAuthorization();
 
